e431d4aa5266872d8de71b8d0171c3db327506cd
Erwin Ramadhan <erwinramadhan35@gmail.com>
2021-02-07 15:00:04 +0700

    [Erwin] Belajar dirumah

---
+++ controller/mahasiswaController.js
@@ -0,0 +1,96 @@
+let mahasiswa = [
+    {
+        nim: 1,
+        nama: 'erwin',
+        jurusan: 'TP'
+    },
+    {
+        nim: 2,
+        nama: 'aa',
+        jurusan: 'ilkom'
+    },
+    {
+        nim: 3,
+        nama: 'bb',
+        jurusan: 'TP'
+    }
+];
+
+module.exports = {
+    getMahasiswaAll: (req, res)=>{
+        const getMahasiswa = req.params
+        if (mahasiswa.length > 0) {
+            res.json({
+                status: true,
+                method: req.method,
+                url: req.url,
+                mahasiswa
+            })
+        } else {
+            res.json({
+                status: false,
+                message: 'data mahasiswa masih kosong'
+            })
+        }
+    },
+    getMahasiswaByNim: (req,res)=>{
+        const nim = +req.params.nim; //bikin variabel untuk menyimpan +parameter nim
+        const dataMhs = mahasiswa.find(mhs => mhs.nim === nim); //bikin variable yang menyimpan fungsi mencari data melalui nim yang telah diinputkan di route
+
+        if (dataMhs != null){
+            return res.status(200).json({
+                success: true,
+                method: req.method,
+                url: req.url,
+                message: `data mahasiswa dengan nim:${nim}`,
+                dataMhs
+            })
+        } else{
+            res.json({
+                status: false,
+                message: `mahasiswa dengan nim ${nim} belum ada data`
+            })
+        }       
+    },
+    postMahasiswa:(req, res) => {
+        mahasiswa.push(req.body);
+        res.send({
+            status: true,
+            data: mahasiswa,
+            method: req.method,
+            url: req.url,
+            message: 'Data mahasiswa berhasil disimpan'
+        })
+    },
+    updateMahasiswa: (req, res)=>{
+        const nim = req.params.nim;
+        const nama = req.body.nama;
+        const jurusan = req.body.jurusan;
+        mahasiswa.filter(mhs => {
+            if (mhs.nim == nim) {
+                mhs.nim = nim;
+                mhs.nama = nama;
+                mhs.jurusan = jurusan;
+                return mhs;
+            }
+        })
+        res.json({
+            status: true,
+            method: req.method,
+            mahasiswa,
+            message: `Mahasiswa dengan NIM: ${nim} berhasil di update`,
+            url: req.url
+        })
+    },
+    deleteMahasiswa: (req,res)=>{
+        let nim = req.params.nim;
+        mahasiswa = mahasiswa.filter(mhs => mhs.nim != nim)
+        res.send({
+            status: true,
+            method: req.method,
+            url: req.url,
+            message: `Mahasiswa dengan nim ${nim} berhasil dihapus`,
+            mahasiswa
+        })
+    }
+};
--- router/mahasiswaRouter.js
+++ router/mahasiswaRouter.js
@@ -1,52 +1,14 @@
 const express = require('express');
+const mahasiswaController = require('../controller/mahasiswaController');
 const router = express.Router();
-const app = express();
-
-let mahasiswa = [{
-    nim:1,
-    nama:'erwin',
-    jurusan:'TP'
-},
-{
-    nim:2,
-    nama:'aa',
-    jurusan:'ilkom'
-},
-{
-    nim:3,
-    nama:'bb',
-    jurusan:'TP'
-}]
 
 router.route('/mahasiswa')
-    .get((req, res)=>{
-    const getMahasiswa = req.params
-     res.json(mahasiswa)
- })
-    .post((req, res)=>{
-    mahasiswa.push(req.body);
-    console.log(req.body)
-     res.send(req.body);
- })
+    .get(mahasiswaController.getMahasiswaAll)
+    .post(mahasiswaController.postMahasiswa)
 
- router.put('/mahasiswa/:nim', (req, res)=>{
-    const nim = req.params.nim;
-    const nama = req.body.nama;
-    const jurusan = req.body.jurusan;
-    mahasiswa.filter(mhs =>{
-        if(mhs.nim == nim){
-            mhs.nim = nim;
-            mhs.nama = nama;
-            mhs.jurusan = jurusan;
-            return mhs;
-        }
-    })
-    res.send(mahasiswa)
-})
-
-router.delete('/mahasiswa/:nim', (req, res)=>{
-    const deleteMhs = req.params.nim
-    res.send(deleteMhs)
-})
+router.route('/mahasiswa/:nim')
+    .get(mahasiswaController.getMahasiswaByNim)
+    .put(mahasiswaController.updateMahasiswa)
+    .delete(mahasiswaController.deleteMahasiswa)
 
 module.exports = router
